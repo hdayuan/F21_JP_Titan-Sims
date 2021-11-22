81c1f5fba5ce21322ea2ccf54fe16ed552df289d
hdayuan <70733594+hdayuan@users.noreply.github.com>
2021-11-21 22:45:46 -0500

    try inclination

--- Titan-tides-sims.py
+++ Titan-tides-sims.py
@@ -1,7 +1,12 @@
 """Description:
-Takes 4 command line arguments: number of samples, initial and 
-final semi-major axes of titan in Saturn radii, and factor to divide
-timescale by
+
+To run:
+
+python3 Titan-sims.py [number of samples] [initial a in Saturn radii]
+[target final a in Saturn radii]
+
+Takes 3 command line arguments: number of samples, initial and
+final semi-major axes of titan in Saturn radii
 Prints these four parameters in the order above,
 then calls main() (see below for description of main()). Finally, prints out
 total time for the program to run."""
@@ -13,33 +18,40 @@ import numpy as np
 import time
 
 
-"""4 parameters: numSamples is number of samples over integration, 
-ia_titan and fa_titan are initial and final semi-major axes of Titan 
+"""4 parameters: numSamples is number of samples over integration,
+ia_titan and fa_titan are initial and final semi-major axes of Titan
 in Saturn radii respectively, file is the file to which output
 is written
 
-Integrates the system from a = ia_titan to a = fa_titan and prints 
-numSamples data points, each on one line in the following format: 
+Integrates the system from a = ia_titan to a = fa_titan and prints
+numSamples data points, each on one line in the following format:
 
 'semi-major axis in Saturn radii'[tab]'eccentricity'[tab]'longitude of pericenter'
 [tab]'mean anomaly of Sun's "orbit" around Saturn'[tab]'current time in simulation'
 """
-def main(numSamples, ia_titanRS, fa_titanRS, file, factor):
+def main(numSamples, ia_titanRS, fa_titanRS, file):
 
     # Constants
     G = 6.67e-11 # G in SI units ****
     mSun = 1.9891e30 # mass of sun in kg ****
     AU_TO_M = 1.496e+11 # meters in one AU
+    YR_TO_SEC = 365.25*24.*3600. # seconds in a year
 
     # Check accuracy of all the following constants
     mSat = 0.0002857 # mass of saturn in solar masses
     aSat = 9.5549 # semi-major axis of Saturn's orbit in AU
     eSat = 0.0565 # eccentricity of Saturn
     rSat = 0.00038926024 # radius of Saturn in AU
-    j2Sat = 16298e-6 # J2 of Saturn (Murray and Dermott p 531) 
+    oSat = 26.7 * np.pi / 180. # obliquity of Saturn in radians
+    j2Sat = 16298e-6 # J2 of Saturn (Murray and Dermott p 531)
+    QSat = 5000. # Tidal Q factor of Saturn (Lainey et al.)
+    k2Sat = 0.39 # Love number of Saturn *** CHECK THIS ***
     curr_aTitan = 0.008167696467 # modern-day semi-major axis of titan in AU
     mTitan = 0.0000000676319759 # mass of titan in solar masses
-    eTitan = 0.001 # eccentricity of Titan's orbit ***** Modify as needed *****
+    eTitan = 0.001 # initial eccentricity of Titan's orbit ***** Modify as needed *****
+    rTitan = 0.04421567543 * rSat # radius of Titan in AU
+    k2Titan = 0.15 # Love number of Titan *** CHECK THIS ***
+    QTitan = 100. # Tidal Q factor of Titan
 
     ia_titanAU = ia_titanRS * rSat  # starting semi-major axis of Titan
 
@@ -51,7 +63,7 @@ def main(numSamples, ia_titanRS, fa_titanRS, file, factor):
     # calculate period of Titan in years at evection resonance
     # distance ** Check accuracy **
     mm = np.sqrt(G*mSat*mSun / ((exp_aResRS*rSat*AU_TO_M)**3))  # in rad / sec
-    tauTitan = 2*np.pi*(1/mm)/(3600*24*365.25) # in years
+    tauTitan = 2*np.pi*(1/mm)/YR_TO_SEC # in years
 
     # Initialize rebound simulation
     sim = rebound.Simulation()
@@ -65,30 +77,52 @@ def main(numSamples, ia_titanRS, fa_titanRS, file, factor):
     # add Titan
     sim.add(m=mTitan, a=ia_titanAU, e=eTitan, hash = "Titan")
 
-    # add sun (with semi-major axis and eccentricity of Saturn)
+    # add Sun (with semi-major axis and eccentricity of Saturn, inclination = obliquity of Saturn)
     sim.add(m=1., a=aSat, e=eSat, hash = "Sun")
-
-    # Initiate reboundx
-    rebx = reboundx.Extras(sim)
 
     # Calculate timescale for exponential migration of Titan's semi-major axis
     ageSat = 4.503e9 # age of saturn in yrs
-    timescale = (1./factor) * 3. * ((exp_aResRS*rSat/curr_aTitan)**3) * ageSat
+    timescale = 3 * ((exp_aResRS*rSat/curr_aTitan)**3) * ageSat
 
     # calculate total time to integrate based on ia_titan, fa_titan, tau, and
     # given exponential migration of form a = a_0 * e^(t/tau)
-    totSimTime = timescale * np.log(fa_titanRS / ia_titanRS) 
+    totSimTime = timescale * np.log(fa_titanRS / ia_titanRS)
+
+    # Initiate reboundx
+    rebx = reboundx.Extras(sim)
+    ps = sim.particles
 
     # Add migration force for Titan's outward migration (a = a0e^(t/tau)
-    mof = rebx.load_force("modify_orbits_forces")
-    rebx.add_force(mof)
-    sim.particles["Titan"].params["tau_a"] = timescale
+    # mof = rebx.load_force("modify_orbits_forces")
+    # rebx.add_force(mof)
+    #ps["Titan"].params["tau_a"] = timescale
 
     # add Saturn's J2
     gh = rebx.load_force("gravitational_harmonics")
     rebx.add_force(gh)
-    sim.particles["Saturn"].params["J2"] = j2Sat
-    sim.particles["Saturn"].params["R_eq"] = rSat
+    ps["Saturn"].params["J2"] = j2Sat
+    ps["Saturn"].params["R_eq"] = rSat
+
+    # add tidal forces
+    tides = rebx.load_force("tides_constant_time_lag")
+    rebx.add_force(tides)
+
+    # Tidal forces of Titan
+    titanT = 2*mm*QTitan*(rTitan*AU_TO_M)**3 / (G*mTitan*mSun*YR_TO_SEC) # in years
+    print(titanT)
+    ps["Titan"].r = rTitan # AU
+    ps["Titan"].params["tctl_k2"] = k2Titan
+    ps["Titan"].params["tctl_tau"] = titanT
+    ps["Titan"].params["Omega"] = mm*YR_TO_SEC # in radians per year
+
+    # Tidal forces of Saturn
+    nmSat = np.sqrt(G*mSun / ((aSat*AU_TO_M)**3))  # in rad / sec
+    satT = 2*nmSat*QSat*(rSat*AU_TO_M)**3 / (G*mSat*mSun*YR_TO_SEC) # in years
+    print(satT)
+    ps["Saturn"].r = rSat # AU
+    ps["Saturn"].params["tctl_k2"] = k2Sat
+    ps["Saturn"].params["tctl_tau"] = satT
+    ps["Saturn"].params["Omega"] = 2*np.pi*YR_TO_SEC/(10.656*3600) # in rad per year
 
     plotDT = totSimTime/numSamples
 
@@ -98,12 +132,12 @@ def main(numSamples, ia_titanRS, fa_titanRS, file, factor):
         # move to Saturn's frame of reference
         sim.move_to_hel()
         # write output
-        file.write(str(sim.particles["Titan"].a/rSat)+"\t"+str(sim.particles["Titan"].e)+"\t")
-        file.write(str(sim.particles["Titan"].pomega)+"\t"+str(sim.particles["Sun"].l))
-        file.write("\t"+str(sim.t)+"\n")
+        file.write(str(ps["Titan"].a/rSat)+"\t"+str(ps["Titan"].e)+"\t")
+        file.write(str(ps["Titan"].pomega)+"\t"+str(ps["Sun"].l)+"\t")
+        file.write(str(ps["Titan"].inc)+"\t")
+        file.write(str(sim.t)+"\n")
 
     # sim.cite()
-
 
 
 # start timer
@@ -113,19 +147,17 @@ start_time = time.time()
 numSamples = int(sys.argv[1])
 ia_titan = float(sys.argv[2])
 fa_titan = float(sys.argv[3])
-div_factor = float(sys.argv[4])
 
 # open file
-f = open("v2-div"+str(div_factor)+"-output-"+str(numSamples)+"s-"+str(ia_titan)+"to"+str(fa_titan)+"rs.txt", "a")
+f = open("v3-tides-output-"+str(numSamples)+"s-"+str(ia_titan)+"to"+str(fa_titan)+"rs.txt", "a")
 
 # Write parameters of simulation
 f.write(str(numSamples)+"\n")
 f.write(str(ia_titan)+"\n")
 f.write(str(fa_titan)+"\n")
-f.write(str(div_factor)+"\n")
 
 # call main
-main(numSamples, ia_titan, fa_titan, f, div_factor)
+main(numSamples, ia_titan, fa_titan, f)
 
 # Write running time
 totTimeSec = time.time() - start_time
@@ -136,4 +168,3 @@ f.write("Running time: "+str(numHours)+ " hours "+str(numMins)+" minutes "+str(n
 
 # close the file
 f.close()
-
